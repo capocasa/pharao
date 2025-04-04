2771a45010a253372fabb0727fa5a1f3b208e2b1
Carlo Capocasa <carlo@capocasa.net>
2025-04-04 17:14:41 +0200

    undo unique lib id reload, fix was actually path

--- pharao.nim
+++ pharao.nim
@@ -80,23 +80,20 @@ PHARAOH_LOG_ERRORS       true
 
     createDir(dynlibRoot)
 
-    const DynlibFormatWithId = DynlibFormat & ".$2"
-    const DynlibPatternWithId = DynlibFormatWithId.replace("$1", "$+").replace("$2", "$i")
+    const DynlibPattern = DynlibFormat.replace("$1", "$+")
 
     for dynlibPath in walkDirRec(dynlibRoot):
       let dynlibName = dynlibPath.lastPathPart
       var name: string
-      var uniqueId: int
-      if scanf(dynlibName, DynlibPatternWithId, name, uniqueId):
-        let path = dynlibPath.parentDir[ dynlibRoot.len .. ^1 ] / name
-        let route = PharaoRoute(path: path)
+      if scanf(dynlibName, DynlibPattern, name):
+        let path = dynlibPath.parentDir[ dynlibRoot.len .. ^1 ] & DirSep & name
+        let route = newPharaoRoute(path)
         route.libHandle = loadLib(dynlibPath)
         route.requestProc = cast[RequestProc](route.libHandle.symAddr("request"))
         if route.requestProc.isNil:
           logger.error("Invalid dynamic library $1, not loading route for path $2" % [dynlibPath, path])
         else:
           route.libModificationTime = dynlibPath.getLastModificationTime
-          route.uniqueId = uniqueId
           routes[path] = route
 
     proc handler(request: Request) =
@@ -104,15 +101,11 @@ PHARAOH_LOG_ERRORS       true
       let defaultHeaders = @{"Content-Type": "text/plain"}.HttpHeaders
       if fileExists(sourcePath):
 
-        var route = routes.mgetOrPut(request.path, PharaoRoute(path: request.path))
+        var route = routes.mgetOrPut(request.path, newPharaoRoute(request.path))
         if route.libModificationTime < sourcePath.getLastModificationTime:
           let (dir, name, ext) = request.path.splitFile
-          let uniqueId = route.uniqueId + 1
           
-          proc makeDynlibPath(uniqueId: int): string =
-            dynlibRoot / request.path.parentDir / DynlibFormatWithId % [request.path.lastPathPart, $uniqueId]
-
-          let dynlibPath = makeDynlibPath(uniqueId)
+          let dynlibPath = dynlibRoot / request.path.parentDir / DynlibFormat % request.path.lastPathPart
           createDir(dynlibPath.parentDir)
 
           # compile the source.
@@ -120,7 +113,7 @@ PHARAOH_LOG_ERRORS       true
 
           if not route.libHandle.isNil:
             route.libHandle.unloadLib
-          let cmd = "$# c --nimcache:$# --app:lib --d:useMalloc --d:pharaoh.sourcePath=$# -o:$# $#" % [nimCmd, nimCachePath, sourcePath, dynlibPath, "-"]
+          let cmd = "$1 c --nimcache:$2 --app:lib --d:useMalloc --d:pharaoh.sourcePath=$3 -o:$4 -" % [nimCmd, nimCachePath, sourcePath, dynlibPath]
           let (output, exitCode) = execCmdEx(cmd, input="include pharao/wrap")
           if exitCode != 0:
             if outputErrors:
